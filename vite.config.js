2d69d602a189e7e2d442bd1b9bed82d8117570af
ez003612 <ez003612@gmail.com>
2024-09-10 17:41:27 +0800

    manifest 更新

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/note_app/',
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
